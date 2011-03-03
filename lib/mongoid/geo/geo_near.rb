f716aa9484be067261c9bb9cb04e153455f1e552
Kristian Mandrup <kmandrup@gmail.com>
2011-03-03 16:00:33 +0100

    improved geoNear with more options and ability to return models array

--- lib/mongoid/geo/geo_near.rb
+++ lib/mongoid/geo/geo_near.rb
@@ -5,22 +5,48 @@ require 'mongoid/geo/haversine'
 
 module Mongoid
   module Geo
+    module Model
+      def to_model
+        clazz.where(:_id => _id).first
+      end
+    end
+    
+    module Models
+      def to_models
+        map do |m|
+          m.to_model
+        end
+      end
+    end
+    
     module Near
-      def geoNear(center, location_attribute, mode = :plane)
+      def geoNear(center, location_attribute, options = {})
         center = center.respond_to?(:collection) ? center.send(location_attribute) : center
-        query = create_query(self, center, mode)
-        create_result(query_result(self, query, center, location_attribute))
+        query = create_query(self, center, options)
+        create_result(query_result(self, query, center, location_attribute)).extend(Mongoid::Geo::Models)
       end
 
       protected
     
-      def create_query clazz, center, mode = :plane
-        BSON::OrderedHash.new.tap do |query|
-          query["geoNear"]    = clazz.to_s.tableize
-          query["near"]       = center
+      def create_query clazz, center, options = {}
+        num                 = options[:num]
+        maxDistance         = options[:maxDistance]
+        query               = options[:query]
+        distanceMultiplier  = options[:distanceMultiplier]        
+        mode                = options[:mode] || :plane
+
+        nq = BSON::OrderedHash.new.tap do |near_query|
+          near_query["geoNear"]      = clazz.to_s.tableize
+          near_query["near"]         = center
+          near_query["num"]          = num if num
+          near_query["maxDistance"]  = maxDistance if maxDistance
+          near_query["distanceMultiplier"]  = distanceMultiplier if distanceMultiplier
+          near_query["query"]        = query if query
+
           # works in mongodb 1.7 but still in beta and not supported by mongodb
-          query["spherical"]  = true if mode == :sphere
+          near_query["spherical"]  = true if mode == :sphere
         end
+        nq
       end      
 
       def query_result clazz, query, center, location_attribute
@@ -28,13 +54,15 @@ module Mongoid
         query_result = clazz.collection.db.command(query)['results'].sort_by do |r|
           loc = r['obj'][location_attribute.to_s]
           r['distance'] = Mongoid::Geo::Haversine.distance(lat, lon, loc[1], loc[0])
+          r['clazz'] = clazz
         end
       end
 
       def create_result query_result
         query_result.inject([]) do |result, qr|
-          res = Hashie::Mash.new(qr['obj'].to_hash)
-          res.dis = qr['distance']
+          res = Hashie::Mash.new(qr['obj'].to_hash).extend(Mongoid::Geo::Model)
+          res.clazz = qr['clazz']
+          res.distance = qr['distance']
           res._id = qr['obj']['_id'].to_s
           result.push(res)
         end
--- spec/mongoid/geo/geo_near_spec.rb
+++ spec/mongoid/geo/geo_near_spec.rb
@@ -2,26 +2,61 @@ require "mongoid/spec_helper"
 
 Address.collection.create_index([['location', Mongo::GEO2D]], :min => -180, :max => 180)
 
+
+Address.create(:location => [45, 11], :city => 'Munich')
+Address.create(:location => [46, 12], :city => 'Berlin')
+
 describe Mongoid::Geo::Near do
 
   let(:address) do
     Address.new        
   end  
 
-  before do
-    Address.create(:location => [45, 11])
-    Address.create(:location => [46, 12])
-  end
-
   describe "geoNear" do
     it "should work with specifying specific center and different location attribute on collction" do
       address.location = "23.5, -47"
-      puts Address.geoNear(address.location, :location)
+      Address.geoNear(address.location, :location).first.location[0].should == 45
     end
-
+    
     it "should assume same attribute searched on both center instance and collection" do
       address.location = "23.5, -47"
-      puts Address.geoNear(address, :location)
+      Address.geoNear(address, :location).first.location[0].should == 45
+    end
+
+    describe '#to_models' do
+      it "should limit number of results to 1" do
+        address.location = "23.5, -47"
+        models = Address.geoNear(address, :location, :num => 1).to_models
+        models.first.lat.should == 45
+      end
+    end
+
+    describe 'option :num' do
+      it "should limit number of results to 1" do
+        address.location = "23.5, -47"
+        Address.geoNear(address, :location, :num => 1).size.should == 1
+      end
+    end
+    
+    describe 'option :maxDistance' do
+      it "should limit on maximum distance" do
+        address.location = "45.1, 11.1"
+        Address.geoNear(address, :location, :maxDistance => 0.2).size.should == 1
+      end
+    end
+    
+    describe 'option :distanceMultiplier' do
+      it "should multiply returned distance with multiplier" do
+        address.location = "45.1, 11.1"
+        Address.geoNear(address, :location, :distanceMultiplier => 4).map(&:distance).first.should > 10
+      end
+    end
+    
+    describe 'option :query' do
+      it "should filter using extra query option" do
+        address.location = "45.1, 11.1"
+        Address.geoNear(address, :location, :query => {:city => 'Munich'}).size.should == 1
+      end
     end
   end
 end
