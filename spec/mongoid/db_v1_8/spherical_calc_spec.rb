6bec9349fb3544f6027c9c02cfb0331bf0672e1a
niedhui <dianhui.nie@gmail.com>
2011-05-04 14:58:50 +0800

    make pherical_calc_spec pass

--- spec/mongoid/db_v1_8/spherical_calc_spec.rb
+++ spec/mongoid/db_v1_8/spherical_calc_spec.rb
@@ -18,27 +18,21 @@ describe 'Mongoid Spherical mode' do
       end
 
       before :each do
-        # @center = Address.create(:location => {:lng => 31.2010839, :lat => -121.583509}, :city => 'center')
         @center = Address.create(:location => {:lat => 31.2010839, :lng => -121.583509}, :city => 'center')
-
-        # @icc = Address.create(:location => {:lng => 31.2026708, :lat => -121.6024088}, :city => 'icc')
         @icc = Address.create(:location => {:lat => 31.2026708, :lng => -121.6024088}, :city => 'icc')
       end
     
       it " cal distance" do
-        # d1 = Geokit::LatLng.distance_between(@center.location,@icc.location,{:units=> :kms}) # 1.8078417965905265
-        # d2 = Geokit::LatLng.distance_between(@center.location,@icc.location,{:units=> :kms,:formula => :flat}) # 1.5037404243943175
 
         Mongoid::Geo.mongo_db_version.should == 1.8
         Mongoid::Geo.spherical.should be_true
         
-        results = Address.geoNear @center.location, :location, :distanceMultiplier => 100
-        # puts "near results: #{results}"
+        results = Address.geoNear @center.location, :location, :distanceMultiplier => 6371,:mode => :sphere
 
         c = results.map(&:distance)  
-        # puts "c: #{c}"
-        # # 1.8062052078680642
         hd =  Mongoid::Geo::Haversine.distance(@center.lat, @center.lng, @icc.lat, @icc.lng) * 6371
+        # c is  1.8061253521165859
+        # hd is 1.8062052078680642
         c.last.should be_within(0.5).of(hd)
       end      
     end
