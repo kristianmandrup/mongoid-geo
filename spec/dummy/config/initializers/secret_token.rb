bd54832828900109301506dc06617be33895edcb
salbito <salbito@sca-webdev2.localdomain>
2011-05-18 17:49:52 -0700

    Removal of all tests to start clean

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '601d20832f20a4264f333ac191e1c2f31c77aa5cf00e82fd18181d807f52cbde8c9a2449031d18c81136826d23c4bd2fed0e0bbfca4e81695b7312b0b85c4fc3'
--- spec/models/person.rb
+++
@@ -1,9 +0,0 @@
-class Person
-  include Mongoid::Document
-
-  field       :name
-  embeds_many :addresses
-    
-  index :addresses
-  index :name
-end
